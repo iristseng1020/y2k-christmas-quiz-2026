5aa7a4fa7e783b8536bfad9aa59348ea17000463
iristseng1020 <iris.tseng@pwc.com>
2026-09-08 20:17:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-window.onload = function() {
-  alert("HELLO");
-}
+window.onload = function () {
+
+  const startBtn =
+    document.getElementById("startBtn");
+
+  startBtn.addEventListener(
+    "click",
+    function () {
+
+      alert("按鈕成功");
+
+    }
+  );
+
+};
+``
